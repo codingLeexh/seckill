ac6395140bbec184d095f209d94146de0fac5865
codingLeexh <codingLeexh@163.com>
2018-02-23 16:30:52 +0800

    秒杀系统完整源码

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -5,16 +5,16 @@ CREATE DATABASE seckill;
 -- 使用数据库
 use seckill;
 CREATE TABLE seckill(
-  `seckill_id` BIGINT NOT NUll AUTO_INCREMENT COMMENT '商品库存ID',
+  `seckillid` BIGINT NOT NUll AUTO_INCREMENT COMMENT '商品库存ID',
   `name` VARCHAR(120) NOT NULL COMMENT '商品名称',
   `number` int NOT NULL COMMENT '库存数量',
-  `start_time` TIMESTAMP  NOT NULL COMMENT '秒杀开始时间',
-  `end_time`   TIMESTAMP   NOT NULL COMMENT '秒杀结束时间',
-  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (seckill_id),
-  key idx_start_time(start_time),
-  key idx_end_time(end_time),
-  key idx_create_time(create_time)
+  `starttime` TIMESTAMP  NOT NULL COMMENT '秒杀开始时间',
+  `endtime`   TIMESTAMP   NOT NULL COMMENT '秒杀结束时间',
+  `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (seckillid),
+  key idx_start_time(starttime),
+  key idx_end_time(endtime),
+  key idx_create_time(createtime)
 )ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
 
 -- 初始化数据
